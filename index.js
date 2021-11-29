6b259e7d5e73f78f494ffc5d145eb707f6baa7e6
maheshpatil132 <92781925+maheshpatil132@users.noreply.github.com>
2021-11-29 15:48:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+let menu = document.getElementsByClassName("menu")[0]
+let menu_btn = document.getElementsByClassName("fa-bars")[0]
+let header = document.getElementsByClassName("header")[0]
+let search = document.getElementsByClassName("seacrh_btn")[0]
+let searchbar = document.getElementsByClassName("search")[0]
+let icons = document.getElementsByClassName("icons")[0]
+let back = document.getElementsByClassName("back")[0]
+menu.addEventListener('click',function(){
+  header.classList.toggle("active")
+})
+search.addEventListener('click',function(){
+    searchbar.classList.add("active")
+})
+back.addEventListener('click',function(){
+    searchbar.classList.remove("active")
+})
