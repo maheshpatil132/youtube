8da7e49940e690b7fc09e3fc6cf5166a544274b9
maheshpatil132 <92781925+maheshpatil132@users.noreply.github.com>
2021-11-29 18:17:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+let body = document.getElementsByTagName("body")[0]
 let menu = document.getElementsByClassName("menu")[0]
 let menu_btn = document.getElementsByClassName("fa-bars")[0]
 let header = document.getElementsByClassName("header")[0]
@@ -7,10 +8,13 @@ let icons = document.getElementsByClassName("icons")[0]
 let back = document.getElementsByClassName("back")[0]
 menu.addEventListener('click',function(){
   header.classList.toggle("active")
+  body.classList.toggle("active")
 })
 search.addEventListener('click',function(){
     searchbar.classList.add("active")
+    body.classList.toggle("active")
 })
 back.addEventListener('click',function(){
     searchbar.classList.remove("active")
+    body.classList.toggle("active")
 })
